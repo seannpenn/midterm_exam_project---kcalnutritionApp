ebc6f79cf63b5f98069b9f3e1fe22917c96eb6ce
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-11-14 00:15:10 +0800

    OnBoardingPage modification (Partial)

--- flutter_application_kcalnutritionapp/lib/screens/onBoarding.dart
+++ flutter_application_kcalnutritionapp/lib/screens/onBoarding.dart
@@ -1,26 +1,37 @@
 
-
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_application_kcalnutritionapp/screens/onBoardingScreens/AllScreen.dart';
+
 
 class onBoardingPage extends StatelessWidget{
   @override
   Widget build(BuildContext context)=> SafeArea(
+
+    
     child: IntroductionScreen(
+      
       pages: [
+        
         PageViewModel(
-          title: "Title of first page",
-          body: "Here you can write the description of the page, to explain someting...",
+          titleWidget: customTitleText(),
+          // body: "Here you can write the description of the page, to explain someting...",
+          bodyWidget: firstScreen(),
+          // image: introImage('assets/onBoard1assets/onBoard1.png.png'),
+          footer: customButton(),
+          
           
         ),
         PageViewModel(
-          title: 'Krystel gwapa kaayo',
-          body: 'Iloveyou Krystel',
+          titleWidget: customTitleText(),
+          bodyWidget: secondScreen(),
+          footer: customButton(),
           
         ),
         PageViewModel(
-          title: 'Krystel gwapa kaayo',
-          body: 'Iloveyou Krystel',
+          titleWidget: customTitleText(),
+          bodyWidget: thirdScreen(),
+          footer: customButton(),
           
         ),
       ],
@@ -31,21 +42,71 @@ class onBoardingPage extends StatelessWidget{
       done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600)), 
       onDone: () => (){},
       
-  //     showSkipButton: true,
-  //     skip: const Icon(Icons.skip_next),
-  //     next: const Icon(Icons.next),
-  //     done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-  //     dotsDecorator: DotsDecorator(
-  //     size: const Size.square(10.0),
-  //     activeSize: const Size(20.0, 10.0),
-  //     activeColor: theme.accentColor,
-  //     color: Colors.black26,
-  //     spacing: const EdgeInsets.symmetric(horizontal: 3.0),
-  //     activeShape: RoundedRectangleBorder(
-  //     borderRadius: BorderRadius.circular(25.0)
-  //   )
-  // ),
+
     )
   );
-  
+
+}
+  // Widget introImage(String assetName) {
+  // //widget to show intro image
+  //   return Align(
+  //     child: Image.asset('$assetName', width: 350.0),
+  //     alignment: Alignment.center,
+  //   );
+  // }
+
+class customButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          // margin: EdgeInsets.only(top: 80.0, bottom: 0),
+          child: 
+              Material(
+              // elevation: 8,
+              
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.redAccent[100],
+              child: MaterialButton(
+                minWidth: 350,
+                height: 70,
+                padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                onPressed: (){}, // add this here
+                child: Text('Get Started',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30)
+                        .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
+                        
+              ),
+              
+            ),
+        ),
+
+        Text('Already have an account? Log In',
+        style: TextStyle(
+          fontSize: 25
+        ),)
+      ],
+    );
+    
+  }
+}
+class customTitleText extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: 
+        Text("kcal",
+          style: 
+            TextStyle(
+              color: Colors.green,
+              fontFamily: 'rounded(2)',
+              fontSize: 50,
+            ),
+        ),
+    );
+  }
 }
