9657434f350f0318e0cd16b353a98efa8e95415b
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-11-16 16:47:06 +0800

    added tab bar in favorites

--- flutter_application_kcalnutritionapp/lib/layout/defaultLayoutPages/favorites.dart
+++ flutter_application_kcalnutritionapp/lib/layout/defaultLayoutPages/favorites.dart
@@ -1,54 +1,138 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_application_kcalnutritionapp/layout/defaultLayout.dart';
 
-
 class favorites extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // color: Colors.lightBlueAccent,
-      appBar: AppBar(
-       automaticallyImplyLeading: false,
-       centerTitle: true,
-        backgroundColor: Colors.white,
-        title: const 
-        Text('Favorites', style: 
-          TextStyle(
-            color: Colors.black
-            
-            ),
-        ), 
-        elevation: 0,
-      ),
-      body: Column(
-        
-        // mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Center(
-              child: Text('Testing Favorite Page', 
-              textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 40,
-                  
-                  )
+    return MaterialApp(
+        home: DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            new SliverAppBar(
+              backgroundColor: Colors.white,
+              title: Text(
+                'Favorites',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+              centerTitle: true,
+              bottom: TabBar(
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.green,
+                isScrollable: true,
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  color: Colors.green,
+                ),
+                tabs: [
+                  new Container(
+                    width: 120,
+                    height: 50,
+                    child: new Tab(
+                      child: Text(
+                        'Foods',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Tab(child: Text('Foods')),
+                  new Container(
+                    width: 120,
+                    height: 50,
+                    child: new Tab(
+                      child: Text(
+                        'Recipes',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            margin: EdgeInsets.only(top:100),
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            // Icon(Icons.flight, size: 350),
+            foods(),
+            recipes()
+          ],
+        ),
+      )),
+    ));
+  }
+}
+
+class foods extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+    children: [
+      Text(
+        'Krystel gwapa',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 40),
+      ),
+      Container(
+        child: customButton(),
+      )
+    ]);
+  }
+}
+class recipes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    // mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+    // children: [
+      body: Column(
+        children: [
+          Text(
+            'recipes testing',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 40),
           ),
-          Column()
-          // FlatButton(
-          //   onPressed: () {
-          //     Navigator.push(context, MaterialPageRoute(
-          //       builder: (context) => Screen2()
-          //     ));
-          //   },
-          //   child: Text('Go to next screen'),
-          //   color: Colors.white,
-          // ),
         ],
       ),
+      persistentFooterButtons:  <Widget>
+      
+      [
+        
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(bottom:20),
+            child: customButton(),
+          ),
+        )
+      ]
     );
   }
+}
+class customButton extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return Material(
+          // elevation: 8,
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.redAccent[100],
+          child: MaterialButton(
+            minWidth: 320,
+            height: 70,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: (){}, // add this here
+            child: Text('Search',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30, color: Colors.white)
+              ),           
+          ),    
+        );
+  }   
 }
